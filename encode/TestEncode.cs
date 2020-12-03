74a908b428d738ea8fea7724fe483659f7539810
Shahryar haghighifard <shahryar@Shahryars-MacBook-Pro.local>
2020-12-03 13:20:01 -0500

    unit test added, simple refactoring

---
+++ encode/TestEncode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace encode
+{
+    public class TestEncode
+    {
+        private readonly Program _sut;
+
+        public TestEncode()
+        {
+            _sut = new Program();
+        }
+
+        [Fact]
+        public void testEncode()
+        {
+            var input = new byte[] { 0x6A, 0x77, 0xC4};
+
+            string toTest = _sut.Encode(input);
+            Assert.Equal("anfE",toTest);
+
+        }
+    }
+}
